81b255e6cc10249ad748f3f28fd59273b14080ba
fullwall <fullwall25@gmail.com>
2012-11-06 15:50:28 +0800

    These weren't staged

---
+++ src/main/java/com/fullwall/pets/EntityBatPet.java
@@ -0,0 +1,85 @@
+package com.fullwall.pets;
+
+import net.minecraft.server.EntityBat;
+import net.minecraft.server.EntityHuman;
+import net.minecraft.server.World;
+
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.CraftServer;
+import org.bukkit.craftbukkit.entity.CraftBat;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.bukkit.entity.Bat;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+public class EntityBatPet extends EntityBat { // old AI
+    private final Player owner;
+
+    public EntityBatPet(World world, Player owner) {
+        super(world);
+        this.owner = owner;
+        if (owner != null)
+            this.f(false);
+    }
+
+    public EntityBatPet(World world) {
+        this(world, null);
+    }
+
+    private int distToOwner() {
+        EntityHuman handle = ((CraftPlayer) owner).getHandle();
+        return (int) (Math.pow(locX - handle.locX, 2) + Math.pow(locY - handle.locY, 2) + Math.pow(locZ
+                - handle.locZ, 2));
+    }
+
+    @Override
+    protected void bi() {
+        if (owner == null) {
+            super.bi();
+            return;
+        }
+        getNavigation().a(((CraftPlayer) owner).getHandle(), 0.55F);
+        getNavigation().e(); // this is only needed for old ai
+        getControllerMove().c(); // old API
+        getControllerLook().a(); // old API
+        getControllerJump().b(); // etc
+
+        if (distToOwner() > Util.MAX_DISTANCE)
+            this.getBukkitEntity().teleport(owner);
+    }
+
+    @Override
+    public Entity getBukkitEntity() {
+        if (owner != null && bukkitEntity == null)
+            bukkitEntity = new BukkitBatPet(this);
+        return super.getBukkitEntity();
+    }
+
+    public static class BukkitBatPet extends CraftBat implements PetEntity {
+        private final Player owner;
+
+        public BukkitBatPet(EntityBatPet entity) {
+            super((CraftServer) Bukkit.getServer(), entity);
+            this.owner = entity.owner;
+        }
+
+        @Override
+        public Bat getBukkitEntity() {
+            return this;
+        }
+
+        @Override
+        public Player getOwner() {
+            return owner;
+        }
+
+        @Override
+        public void upgrade() {
+        }
+
+        @Override
+        public void setLevel(int level) {
+            // setSize(level);
+        }
+    }
+}
---
+++ src/main/java/com/fullwall/pets/EntitySquidPet.java
@@ -0,0 +1,83 @@
+package com.fullwall.pets;
+
+import net.minecraft.server.EntityHuman;
+import net.minecraft.server.EntitySquid;
+import net.minecraft.server.World;
+
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.CraftServer;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.bukkit.craftbukkit.entity.CraftSquid;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Squid;
+
+public class EntitySquidPet extends EntitySquid { // old AI
+    private final Player owner;
+
+    public EntitySquidPet(World world, Player owner) {
+        super(world);
+        this.owner = owner;
+    }
+
+    public EntitySquidPet(World world) {
+        this(world, null);
+    }
+
+    private int distToOwner() {
+        EntityHuman handle = ((CraftPlayer) owner).getHandle();
+        return (int) (Math.pow(locX - handle.locX, 2) + Math.pow(locY - handle.locY, 2) + Math.pow(locZ
+                - handle.locZ, 2));
+    }
+
+    @Override
+    protected void bk() {
+        if (owner == null) {
+            super.bk();
+            return;
+        }
+        getNavigation().a(((CraftPlayer) owner).getHandle(), 0.55F);
+        getNavigation().e(); // this is only needed for old ai
+        getControllerMove().c(); // old API
+        getControllerLook().a(); // old API
+        getControllerJump().b(); // etc
+
+        if (distToOwner() > Util.MAX_DISTANCE)
+            this.getBukkitEntity().teleport(owner);
+    }
+
+    @Override
+    public Entity getBukkitEntity() {
+        if (owner != null && bukkitEntity == null)
+            bukkitEntity = new BukkitSquidPet(this);
+        return super.getBukkitEntity();
+    }
+
+    public static class BukkitSquidPet extends CraftSquid implements PetEntity {
+        private final Player owner;
+
+        public BukkitSquidPet(EntitySquidPet entity) {
+            super((CraftServer) Bukkit.getServer(), entity);
+            this.owner = entity.owner;
+        }
+
+        @Override
+        public Squid getBukkitEntity() {
+            return this;
+        }
+
+        @Override
+        public Player getOwner() {
+            return owner;
+        }
+
+        @Override
+        public void upgrade() {
+        }
+
+        @Override
+        public void setLevel(int level) {
+            // setSize(level);
+        }
+    }
+}
